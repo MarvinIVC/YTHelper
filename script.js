f7843cb332320c9de9e4c21d2a55ebac302f3c29
MarvinIVC <131504196+MarvinIVC@users.noreply.github.com>
2024-07-15 06:31:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let player;
-let captionsEnabled = false;
 
 function generateVideo() {
     const input = document.getElementById('youtube-link').value;
@@ -9,10 +8,8 @@ function generateVideo() {
     if (videoId) {
         videoContainer.innerHTML = `<div id="player"></div>`;
         onYouTubeIframeAPIReady(videoId);
-        document.getElementById('toggle-captions').style.display = 'inline-block';
     } else {
         videoContainer.innerHTML = '<p>Please enter a valid YouTube link.</p>';
-        document.getElementById('toggle-captions').style.display = 'none';
     }
 }
 
@@ -20,16 +17,6 @@ function onYouTubeIframeAPIReady(videoId) {
     player = new YT.Player('player', {
         height: '315',
         width: '560',
-        videoId: videoId,
-        playerVars: {
-            'cc_load_policy': captionsEnabled ? 1 : 0
-        }
+        videoId: videoId
     });
-}
-
-function toggleCaptions() {
-    captionsEnabled = !captionsEnabled;
-    const videoId = player.getVideoData().video_id;
-    player.destroy();
-    onYouTubeIframeAPIReady(videoId);
 }
